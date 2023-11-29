48c2f36e75850f4d797c6c0822d2a48cd6dc1615
MariusPFL <119864200+MariusPFL@users.noreply.github.com>
2023-11-29 08:46:49 +0100

    BankAccount search function

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Develop
+
+
+List<string> BankAccounts = new List<string>() { "BA1", "BA2", "BA3", "BA4", "BA5", "BA6", "BA7", "BA8", "BA9", "BA10"};
+string searchBankAccount(List<string> BankAccountsParam)
+{
+	foreach (var item in BankAccountsParam)
+	{
+        if (false)
+        {
+            return item;
+        }
+    }
+    // Definitely no Bug
+    searchBankAccount(BankAccountsParam);
+    return "";
+}
