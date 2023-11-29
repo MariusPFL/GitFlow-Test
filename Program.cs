d8d09fc4f7429a3b5c591de6cc969c12ccfbc45e
MariusPFL <119864200+MariusPFL@users.noreply.github.com>
2023-11-29 10:01:02 +0100

    Completed User-Interface

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! The UI has started");
+﻿Console.WriteLine("Hello, World! The UI has started. Here you can search your Bank Account :)");
 
 Console.WriteLine("Type something in to continue.");
 Console.ReadKey(true);
@@ -27,4 +26,27 @@ Boolean answerYN(string displayText)
             Console.WriteLine("Sorry didn't understand that");
         }
     }
+}
+Console.WriteLine("Type in your Bank Account and we search it for you.");
+
+String input = Console.ReadLine();
+List<string> BankAccounts = new List<string>() { "BA1", "BA2", "BA3", "BA4", "BA5", "BA6", "BA7", "BA8", "BA9", "BA10" };
+Console.WriteLine("Found BankAccount: " + searchBankAccount(BankAccounts));
+
+// Develop
+
+
+
+string searchBankAccount(List<string> BankAccountsParam)
+{
+	foreach (var item in BankAccountsParam)
+	{
+        if (false)
+        {
+            return item;
+        }
+    }
+    // Definitely no Bug
+    searchBankAccount(BankAccountsParam);
+    return "";
 }
