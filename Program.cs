9e0bc314919653d407579175a204222c66eda98b
MariusPFL <119864200+MariusPFL@users.noreply.github.com>
2023-11-29 08:37:26 +0100

    Asked if you wanna start the application

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Develop
+Console.WriteLine("Hello, World! The UI has started");
+
+Console.WriteLine("Type something in to continue.");
+Console.ReadKey(true);
+if (!answerYN("Do you wanna access your Bank Account ?"))
+{
+    throw new Exception("Application ended");   
+}
+
+Boolean answerYN(string displayText)
+{
+    Console.WriteLine(displayText);
+    while (true)
+    {
+        String answer = Console.ReadLine().ToLower().Trim(' ');
+        if (answer == "y")
+        {
+            return true;
+        }
+        if (answer == "n")
+        {
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Sorry didn't understand that");
+        }
+    }
+}
